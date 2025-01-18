aefa622026614e5a83ca30bd54275148b185505f
Isak M <isak.morand@gmail.com>
2025-01-18 14:42:33 +0100

    Changed from spaces to tabs

--- Epoch/src/Epoch/Assets/AssetPack/AssetPack.cpp
+++ Epoch/src/Epoch/Assets/AssetPack/AssetPack.cpp
@@ -9,129 +9,129 @@
 
 namespace Epoch
 {
-    static void GetAllReferencedScenes(AssetHandle aCurrentScene, std::unordered_set<AssetHandle>& outReferencedScenes)
-    {
-        std::unordered_set<AssetHandle> referencedScenes;
+	static void GetAllReferencedScenes(AssetHandle aCurrentScene, std::unordered_set<AssetHandle>& outReferencedScenes)
+	{
+		std::unordered_set<AssetHandle> referencedScenes;
 
-        //Load current scene and store all referenced scenes in a set
-        {
-            const AssetRegistry& registry = Project::GetEditorAssetManager()->GetAssetRegistry();
+		//Load current scene and store all referenced scenes in a set
+		{
+			const AssetRegistry& registry = Project::GetEditorAssetManager()->GetAssetRegistry();
 
-            const auto& metadata = registry.Get(aCurrentScene);
-            std::shared_ptr<Scene> scene = std::make_shared<Scene>(aCurrentScene);
-            SceneSerializer serializer(scene);
-            if (serializer.Deserialize(Project::GetAssetDirectory() / metadata.filePath))
-            {
-                //Only add the scene handle to 'referencedScenes' if it's not already in 'outReferencedScenes'
-                std::unordered_set<AssetHandle> sceneReferences = scene->GetAllSceneReferences();
-                for (AssetHandle sceneHandle : sceneReferences)
-                {
-                    if (outReferencedScenes.find(sceneHandle) == outReferencedScenes.end())
-                    {
-                        referencedScenes.insert(sceneHandle);
-                    }
-                }
-            }
-        }
+			const auto& metadata = registry.Get(aCurrentScene);
+			std::shared_ptr<Scene> scene = std::make_shared<Scene>(aCurrentScene);
+			SceneSerializer serializer(scene);
+			if (serializer.Deserialize(Project::GetAssetDirectory() / metadata.filePath))
+			{
+				//Only add the scene handle to 'referencedScenes' if it's not already in 'outReferencedScenes'
+				std::unordered_set<AssetHandle> sceneReferences = scene->GetAllSceneReferences();
+				for (AssetHandle sceneHandle : sceneReferences)
+				{
+					if (outReferencedScenes.find(sceneHandle) == outReferencedScenes.end())
+					{
+						referencedScenes.insert(sceneHandle);
+					}
+				}
+			}
+		}
 
-        outReferencedScenes.insert(referencedScenes.begin(), referencedScenes.end());
+		outReferencedScenes.insert(referencedScenes.begin(), referencedScenes.end());
 
-        //Call this function for each scene in the set
-        for (AssetHandle sceneHandle : referencedScenes)
-        {
-            GetAllReferencedScenes(sceneHandle, outReferencedScenes);
-        }
-    }
+		//Call this function for each scene in the set
+		for (AssetHandle sceneHandle : referencedScenes)
+		{
+			GetAllReferencedScenes(sceneHandle, outReferencedScenes);
+		}
+	}
 
-    bool AssetPack::CreateFromActiveProject(std::atomic<float>& aProgress)
-    {
-        AssetPackFile assetPackFile;
-        assetPackFile.header.buildVersion = Platform::GetCurrentDateTimeU64();
+	bool AssetPack::CreateFromActiveProject(std::atomic<float>& aProgress)
+	{
+		AssetPackFile assetPackFile;
+		assetPackFile.header.buildVersion = Platform::GetCurrentDateTimeU64();
 
-        aProgress = 0.0f;
+		aProgress = 0.0f;
 
-        AssetHandle startSceneHandle = Project::GetActive()->GetConfig().startScene;
-        const AssetRegistry& registry = Project::GetEditorAssetManager()->GetAssetRegistry();
+		AssetHandle startSceneHandle = Project::GetActive()->GetConfig().startScene;
+		const AssetRegistry& registry = Project::GetEditorAssetManager()->GetAssetRegistry();
 
-        if (startSceneHandle == 0 || !registry.Contains(startSceneHandle))
-        {
-            myLastBuildError = BuildError::NoStartScene;
-            return false;
-        }
+		if (startSceneHandle == 0 || !registry.Contains(startSceneHandle))
+		{
+			myLastBuildError = BuildError::NoStartScene;
+			return false;
+		}
 
-        std::unordered_set<AssetHandle> referencedScenes;
-        referencedScenes.insert(startSceneHandle);
-        GetAllReferencedScenes(startSceneHandle, referencedScenes);
-        aProgress += 0.1f;
+		std::unordered_set<AssetHandle> referencedScenes;
+		referencedScenes.insert(startSceneHandle);
+		GetAllReferencedScenes(startSceneHandle, referencedScenes);
+		aProgress += 0.1f;
 
-        float progressIncrement = 0.4f / (float)referencedScenes.size();
+		float progressIncrement = 0.4f / (float)referencedScenes.size();
 
-        std::unordered_set<AssetHandle> fullAssetList;
-        for (AssetHandle sceneHandle : referencedScenes)
-        {
-            const auto& metadata = registry.Get(sceneHandle);
-            std::shared_ptr<Scene> scene = std::make_shared<Scene>(sceneHandle);
-            SceneSerializer serializer(scene);
-            LOG_DEBUG("Deserializing Scene: {}", metadata.filePath);
-            if (serializer.Deserialize(Project::GetAssetDirectory() / metadata.filePath))
-            {
-                std::unordered_set<AssetHandle> sceneAssetList = scene->GetAllSceneAssets();
-                LOG_INFO("  Scene has {} used assets", sceneAssetList.size());
+		std::unordered_set<AssetHandle> fullAssetList;
+		for (AssetHandle sceneHandle : referencedScenes)
+		{
+			const auto& metadata = registry.Get(sceneHandle);
+			std::shared_ptr<Scene> scene = std::make_shared<Scene>(sceneHandle);
+			SceneSerializer serializer(scene);
+			LOG_DEBUG("Deserializing Scene: {}", metadata.filePath);
+			if (serializer.Deserialize(Project::GetAssetDirectory() / metadata.filePath))
+			{
+				std::unordered_set<AssetHandle> sceneAssetList = scene->GetAllSceneAssets();
+				LOG_INFO("  Scene has {} used assets", sceneAssetList.size());
 
-                std::unordered_set<AssetHandle> sceneAssetListWithoutPrefabs = sceneAssetList;
-                for (AssetHandle assetHandle : sceneAssetListWithoutPrefabs)
-                {
-                    const auto& metadata = Project::GetEditorAssetManager()->GetMetadata(assetHandle);
-                    if (metadata.type == AssetType::Prefab)
-                    {
-                        std::shared_ptr<Prefab> prefab = AssetManager::GetAsset<Prefab>(assetHandle);
-                        std::unordered_set<AssetHandle> prefabAssetList = prefab->GetAssetList();
-                        sceneAssetList.insert(prefabAssetList.begin(), prefabAssetList.end());
-                    }
-                }
+				std::unordered_set<AssetHandle> sceneAssetListWithoutPrefabs = sceneAssetList;
+				for (AssetHandle assetHandle : sceneAssetListWithoutPrefabs)
+				{
+					const auto& metadata = Project::GetEditorAssetManager()->GetMetadata(assetHandle);
+					if (metadata.type == AssetType::Prefab)
+					{
+						std::shared_ptr<Prefab> prefab = AssetManager::GetAsset<Prefab>(assetHandle);
+						std::unordered_set<AssetHandle> prefabAssetList = prefab->GetAssetList();
+						sceneAssetList.insert(prefabAssetList.begin(), prefabAssetList.end());
+					}
+				}
 
-                AssetPackFile::SceneInfo& sceneInfo = assetPackFile.indexTable.scenes[sceneHandle];
-                for (AssetHandle assetHandle : sceneAssetList)
-                {
-                    AssetPackFile::AssetInfo& assetInfo = sceneInfo.assets[assetHandle];
-                    const auto& assetMetadata = Project::GetEditorAssetManager()->GetMetadata(assetHandle);
-                    assetInfo.type = (uint16_t)assetMetadata.type;
-                }
+				AssetPackFile::SceneInfo& sceneInfo = assetPackFile.indexTable.scenes[sceneHandle];
+				for (AssetHandle assetHandle : sceneAssetList)
+				{
+					AssetPackFile::AssetInfo& assetInfo = sceneInfo.assets[assetHandle];
+					const auto& assetMetadata = Project::GetEditorAssetManager()->GetMetadata(assetHandle);
+					assetInfo.type = (uint16_t)assetMetadata.type;
+				}
 
-                fullAssetList.insert(sceneAssetList.begin(), sceneAssetList.end());
-            }
-            else
-            {
-                CONSOLE_LOG_ERROR("Failed to deserialize scene: {} ({})", metadata.filePath, sceneHandle);
-            }
+				fullAssetList.insert(sceneAssetList.begin(), sceneAssetList.end());
+			}
+			else
+			{
+				CONSOLE_LOG_ERROR("Failed to deserialize scene: {} ({})", metadata.filePath, sceneHandle);
+			}
 
-            aProgress += progressIncrement;
-        }
+			aProgress += progressIncrement;
+		}
 
-        CONSOLE_LOG_INFO("Project contains {} used assets", fullAssetList.size());
+		CONSOLE_LOG_INFO("Project contains {} used assets", fullAssetList.size());
 
-        return true;
+		return true;
 
-        Buffer appBinary;
-        if (std::filesystem::exists(Project::GetScriptModuleFilePath()))
-        {
-            appBinary = FileSystem::ReadBytes(Project::GetScriptModuleFilePath());
-        }
+		Buffer appBinary;
+		if (std::filesystem::exists(Project::GetScriptModuleFilePath()))
+		{
+			appBinary = FileSystem::ReadBytes(Project::GetScriptModuleFilePath());
+		}
 
-        AssetPackSerializer assetPackSerializer;
-        assetPackSerializer.Serialize(Project::GetAssetDirectory() / "AssetPack.eap", assetPackFile, appBinary, aProgress);
+		AssetPackSerializer assetPackSerializer;
+		assetPackSerializer.Serialize(Project::GetAssetDirectory() / "AssetPack.eap", assetPackFile, appBinary, aProgress);
 
-        aProgress = 1.0f;
-        return true;
-    }
+		aProgress = 1.0f;
+		return true;
+	}
 
-    std::shared_ptr<AssetPack> AssetPack::LoadActiveProject()
-    {
-        return std::shared_ptr<AssetPack>();
-    }
+	std::shared_ptr<AssetPack> AssetPack::LoadActiveProject()
+	{
+		return std::shared_ptr<AssetPack>();
+	}
 
-    const char* AssetPack::GetLastErrorMessage()
-    {
-        return nullptr;
-    }
+	const char* AssetPack::GetLastErrorMessage()
+	{
+		return nullptr;
+	}
 }
--- Epoch/src/Epoch/Assets/AssetPack/AssetPackSerializer.cpp
+++ Epoch/src/Epoch/Assets/AssetPack/AssetPackSerializer.cpp
@@ -5,96 +5,96 @@
 
 namespace Epoch
 {
-    void AssetPackSerializer::Serialize(const std::filesystem::path& aPath, AssetPackFile& aFile, Buffer aAppBinary, std::atomic<float>& aProgress)
-    {
-        LOG_INFO("Serializing AssetPack to {}", aPath.string());
+	void AssetPackSerializer::Serialize(const std::filesystem::path& aPath, AssetPackFile& aFile, Buffer aAppBinary, std::atomic<float>& aProgress)
+	{
+		LOG_INFO("Serializing AssetPack to {}", aPath.string());
 
-        FileStreamWriter serializer(aPath);
-        serializer.WriteRaw<AssetPackFile::FileHeader>(aFile.header);
+		FileStreamWriter serializer(aPath);
+		serializer.WriteRaw<AssetPackFile::FileHeader>(aFile.header);
 
-        // ===============
-        // Write index
-        // ===============
-        // Write dummy data for index (come back later to fill in)
-        uint64_t indexTablePos = serializer.GetStreamPosition();
-        uint64_t indexTableSize = CalculateIndexTableSize(aFile);
-        serializer.WriteZero(indexTableSize);
+		// ===============
+		// Write index
+		// ===============
+		// Write dummy data for index (come back later to fill in)
+		uint64_t indexTablePos = serializer.GetStreamPosition();
+		uint64_t indexTableSize = CalculateIndexTableSize(aFile);
+		serializer.WriteZero(indexTableSize);
 
-        std::unordered_map<AssetHandle, AssetSerializationInfo> serializedAssets;
+		std::unordered_map<AssetHandle, AssetSerializationInfo> serializedAssets;
 
-        float progressIncrement = 0.4f / (float)aFile.indexTable.scenes.size();
+		float progressIncrement = 0.4f / (float)aFile.indexTable.scenes.size();
 
-        // Write app binary data
-        aFile.indexTable.packedAppBinaryOffset = serializer.GetStreamPosition();
-        serializer.WriteBuffer(aAppBinary);
-        aFile.indexTable.packedAppBinarySize = serializer.GetStreamPosition() - aFile.indexTable.packedAppBinaryOffset;
-        aAppBinary.Release();
+		// Write app binary data
+		aFile.indexTable.packedAppBinaryOffset = serializer.GetStreamPosition();
+		serializer.WriteBuffer(aAppBinary);
+		aFile.indexTable.packedAppBinarySize = serializer.GetStreamPosition() - aFile.indexTable.packedAppBinaryOffset;
+		aAppBinary.Release();
 
-        // Write asset data
-        for (auto& [sceneHandle, sceneInfo] : aFile.indexTable.scenes)
-        {
-            // Serialize Scene
-            AssetSerializationInfo serializationInfo;
-            //AssetImporter::SerializeToAssetPack(sceneHandle, serializer, serializationInfo);
-            aFile.indexTable.scenes[sceneHandle].packedOffset = serializationInfo.offset;
-            aFile.indexTable.scenes[sceneHandle].packedSize = serializationInfo.size;
+		// Write asset data
+		for (auto& [sceneHandle, sceneInfo] : aFile.indexTable.scenes)
+		{
+			// Serialize Scene
+			AssetSerializationInfo serializationInfo;
+			//AssetImporter::SerializeToAssetPack(sceneHandle, serializer, serializationInfo);
+			aFile.indexTable.scenes[sceneHandle].packedOffset = serializationInfo.offset;
+			aFile.indexTable.scenes[sceneHandle].packedSize = serializationInfo.size;
 
-            // Serialize Assets
-            for (auto& [assetHandle, assetInfo] : sceneInfo.assets)
-            {
-                if (serializedAssets.find(assetHandle) != serializedAssets.end())
-                {
-                    // Has already been serialized
-                    serializationInfo = serializedAssets.at(assetHandle);
-                    aFile.indexTable.scenes[sceneHandle].assets[assetHandle].packedOffset = serializationInfo.offset;
-                    aFile.indexTable.scenes[sceneHandle].assets[assetHandle].packedSize = serializationInfo.size;
-                }
-                else
-                {
-                    // Serialize asset
-                    //AssetImporter::SerializeToAssetPack(assetHandle, serializer, serializationInfo);
-                    aFile.indexTable.scenes[sceneHandle].assets[assetHandle].packedOffset = serializationInfo.offset;
-                    aFile.indexTable.scenes[sceneHandle].assets[assetHandle].packedSize = serializationInfo.size;
-                    serializedAssets[assetHandle] = serializationInfo;
-                }
-            }
+			// Serialize Assets
+			for (auto& [assetHandle, assetInfo] : sceneInfo.assets)
+			{
+				if (serializedAssets.find(assetHandle) != serializedAssets.end())
+				{
+					// Has already been serialized
+					serializationInfo = serializedAssets.at(assetHandle);
+					aFile.indexTable.scenes[sceneHandle].assets[assetHandle].packedOffset = serializationInfo.offset;
+					aFile.indexTable.scenes[sceneHandle].assets[assetHandle].packedSize = serializationInfo.size;
+				}
+				else
+				{
+					// Serialize asset
+					//AssetImporter::SerializeToAssetPack(assetHandle, serializer, serializationInfo);
+					aFile.indexTable.scenes[sceneHandle].assets[assetHandle].packedOffset = serializationInfo.offset;
+					aFile.indexTable.scenes[sceneHandle].assets[assetHandle].packedSize = serializationInfo.size;
+					serializedAssets[assetHandle] = serializationInfo;
+				}
+			}
 
-            aProgress += progressIncrement;
-        }
+			aProgress += progressIncrement;
+		}
 
-        LOG_INFO("Serialized {} assets into AssetPack", serializedAssets.size());
+		LOG_INFO("Serialized {} assets into AssetPack", serializedAssets.size());
 
-        // Fill index table
-        serializer.SetStreamPosition(indexTablePos);
-        serializer.WriteRaw<uint64_t>(aFile.indexTable.packedAppBinaryOffset);
-        serializer.WriteRaw<uint64_t>(aFile.indexTable.packedAppBinarySize);
+		// Fill index table
+		serializer.SetStreamPosition(indexTablePos);
+		serializer.WriteRaw<uint64_t>(aFile.indexTable.packedAppBinaryOffset);
+		serializer.WriteRaw<uint64_t>(aFile.indexTable.packedAppBinarySize);
 
-        serializer.WriteRaw<uint32_t>((uint32_t)aFile.indexTable.scenes.size()); // Write scene map size
-        for (auto& [sceneHandle, sceneInfo] : aFile.indexTable.scenes)
-        {
-            serializer.WriteRaw<uint64_t>(sceneHandle);
-            serializer.WriteRaw<uint64_t>(sceneInfo.packedOffset);
-            serializer.WriteRaw<uint64_t>(sceneInfo.packedSize);
+		serializer.WriteRaw<uint32_t>((uint32_t)aFile.indexTable.scenes.size()); // Write scene map size
+		for (auto& [sceneHandle, sceneInfo] : aFile.indexTable.scenes)
+		{
+			serializer.WriteRaw<uint64_t>(sceneHandle);
+			serializer.WriteRaw<uint64_t>(sceneInfo.packedOffset);
+			serializer.WriteRaw<uint64_t>(sceneInfo.packedSize);
 
-            serializer.WriteMap(aFile.indexTable.scenes[sceneHandle].assets);
-        }
-    }
+			serializer.WriteMap(aFile.indexTable.scenes[sceneHandle].assets);
+		}
+	}
 
-    bool AssetPackSerializer::DeserializeIndex(const std::filesystem::path& aPath, AssetPackFile& aFile)
-    {
-        return false;
-    }
+	bool AssetPackSerializer::DeserializeIndex(const std::filesystem::path& aPath, AssetPackFile& aFile)
+	{
+		return false;
+	}
 
-    uint64_t AssetPackSerializer::CalculateIndexTableSize(const AssetPackFile& aFile)
-    {
-        uint64_t appBinaryInfoSize = sizeof(uint64_t) * 2; //packedAppBinaryOffset & packedAppBinarySize
-        uint64_t sceneMapSize = sizeof(uint32_t) + (sizeof(AssetHandle) + sizeof(uint64_t) * 2) * aFile.indexTable.scenes.size(); //scene count + (scene handle + scene info) * scene count
-        uint64_t assetMapSize = 0;
-        for (const auto& [sceneHandle, sceneInfo] : aFile.indexTable.scenes)
-        {
-            assetMapSize += sizeof(uint32_t) + (sizeof(AssetHandle) + sizeof(AssetPackFile::AssetInfo)) * sceneInfo.assets.size(); //asset count + (asset handle + asset info) * asset count
-        }
+	uint64_t AssetPackSerializer::CalculateIndexTableSize(const AssetPackFile& aFile)
+	{
+		uint64_t appBinaryInfoSize = sizeof(uint64_t) * 2; //packedAppBinaryOffset & packedAppBinarySize
+		uint64_t sceneMapSize = sizeof(uint32_t) + (sizeof(AssetHandle) + sizeof(uint64_t) * 2) * aFile.indexTable.scenes.size(); //scene count + (scene handle + scene info) * scene count
+		uint64_t assetMapSize = 0;
+		for (const auto& [sceneHandle, sceneInfo] : aFile.indexTable.scenes)
+		{
+			assetMapSize += sizeof(uint32_t) + (sizeof(AssetHandle) + sizeof(AssetPackFile::AssetInfo)) * sceneInfo.assets.size(); //asset count + (asset handle + asset info) * asset count
+		}
 
-        return appBinaryInfoSize + sceneMapSize + assetMapSize;
-    }
+		return appBinaryInfoSize + sceneMapSize + assetMapSize;
+	}
 }
